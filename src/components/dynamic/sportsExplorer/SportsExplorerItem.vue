9b610765aed49206e926278fe56a209da425e51c
Laboratorynotices <46632599+Laboratorynotices@users.noreply.github.com>
2026-02-08 19:29:21 +0100

    feat(sports-explorer): добавлен компонент с тултипами для элемента вида спорта
    В файле src/components/dynamic/sportsExplorer/SportsExplorerItem.vue
    реализован новый компонент, который отображает дополнительную
    информацию через тултипы при наведении на элемент вида спорта.

--- src/components/dynamic/sportsExplorer/SportsExplorerItem.vue
+++ src/components/dynamic/sportsExplorer/SportsExplorerItem.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type { Sport } from "@/types/sportsExplorer";
 import SectionBlockHeading from "@/components/ui/SectionBlock/SectionBlockHeading.vue";
+import UiTooltip from "@/components/ui/UiTooltip.vue";
 
 interface Props {
   sport: Sport;
@@ -10,12 +11,17 @@ defineProps<Props>();
 </script>
 
 <template>
-  <li class="sports-explorer__item" :title="sport.funFact">
-    <div class="sports-explorer__emoji" aria-hidden="true">
-      {{ sport.emoji }}
-    </div>
-    <SectionBlockHeading level="h4" class="sports-explorer__name">
-      {{ sport.name }}
-    </SectionBlockHeading>
+  <li class="sports-explorer__item">
+    <UiTooltip class="sports-explorer__card">
+      <div class="sports-explorer__emoji" aria-hidden="true">
+        {{ sport.emoji }}
+      </div>
+      <SectionBlockHeading level="h4" class="sports-explorer__name">
+        {{ sport.name }}
+      </SectionBlockHeading>
+      <template v-slot:tooltip>
+        {{ sport.funFact }}
+      </template>
+    </UiTooltip>
   </li>
 </template>
