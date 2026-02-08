7030787dc9f241e656c036843250bd8fcd1482e5
Laboratorynotices <46632599+Laboratorynotices@users.noreply.github.com>
2026-02-08 19:12:20 +0100

    feat(ui): добавлен компонент-обёртка UiTooltip для тултипов
    Создан компонент src/components/ui/UiTooltip.vue,
    предоставляющий единый интерфейс для использования тултипов.

---
+++ src/components/ui/UiTooltip.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="tooltip" tabindex="0">
+    <slot />
+    <div class="tooltip__content" role="tooltip" aria-hidden="true">
+      <slot name="tooltip" />
+    </div>
+  </div>
+</template>
